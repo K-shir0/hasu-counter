041fb63080dff1bda4a43f6e13e6b38df06f95f7
k-shir0 <kakishiro2190394@gmail.com>
2024-12-07 22:53:42 +0900

    fix

--- src/hasu_counter/app.py
+++ src/hasu_counter/app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+# Google Tag Manager コード
+gtm_script = f"""
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':
+new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+}})(window,document,'script','dataLayer','GTM-WFPTBT5X');</script>
+<!-- End Google Tag Manager -->
+"""
+
+st.markdown(gtm_script, unsafe_allow_html=True)
+
 hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
